86db6dbd17f337f1d3297c04b1c14f2e58db51f7
c0deb1ind31 <rhutikparab1@gmail.com>
2022-03-26 20:10:22 +0530

    Added Deploy script

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -1,14 +1,42 @@
-
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
+  const TriggerTokenFactory = await ethers.getContractFactory("TriggerToken");
+  const triggerToken = await TriggerTokenFactory.deploy(1000000);
+  await triggerToken.deployed();
 
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const TriggerProtocolFactory = await ethers.getContractFactory(
+    "TriggerProtocol"
+  );
+  const triggerProtocol = await TriggerProtocolFactory.deploy();
+  await triggerProtocol.deployed();
+  const TriggerXpTokenFactory = await ethers.getContractFactory(
+    "TriggerXpToken"
+  );
+  const triggerXpToken = await TriggerXpTokenFactory.deploy(
+    100000000,
+    triggerProtocol.address
+  );
+  await triggerXpToken.deployed();
 
-  await greeter.deployed();
+  const TriggerNftTokenFactory = await ethers.getContractFactory("TriggerNFT");
+  const triggerNftToken = await TriggerNftTokenFactory.deploy();
+  await triggerNftToken.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  const triggerSetAddresstxn = await triggerProtocol.setFactoryAddresses(
+    triggerToken.address,
+    triggerXpToken.address,
+    triggerNftToken.address
+  );
+  await triggerSetAddresstxn.wait();
+
+  console.log("TGR token Contract deployed to:", triggerToken.address);
+  console.log("xTGR token Contract deployed to:", triggerXpToken.address);
+  console.log("NFT token Contract deployed to:", triggerNftToken.address);
+  console.log(
+    "Trigger Protocol token Contract deployed to:",
+    triggerProtocol.address
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
