4157c2e3a2da17365b56acd5ea58c78cdea763e6
c0deb1ind31 <rhutikparab1@gmail.com>
2022-03-26 21:43:47 +0530

    Added explore Portals Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles/globals.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { PortalPage } from "./pages/PortalPage";
+import { ExplorePortalsPage } from "pages/ExplorePortalsPage";
 import { Navbar } from "./Components/Navbar";
 import { Footer } from "./Components/Footer";
 import { PortalCard } from "./Components/PortalCard";
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="dev" element={<PortalCard />} />
           <Route path="/portal/:portalId" element={<PortalPage />} />
+          <Route path="/explore-portals/" element={<ExplorePortalsPage />} />
         </Routes>
       </main>
       {/* <Footer /> */}
---
+++ src/pages/ExplorePortalsPage/ExplorePortalsPage.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Portal } from "Components/Portal";
+import { PortalCard } from "Components/PortalCard";
+
+import styles from "./explore-portals.module.scss";
+export const ExplorePortalsPage = () => {
+  return (
+    <div className={styles.container}>
+      <TrendingPortal />
+      <PortalsList />
+    </div>
+  );
+};
+const TrendingPortal = () => {
+  return (
+    <div className={styles.trending_portal}>
+      <div className={styles.portal}>
+        <Portal />
+      </div>
+      <div className={styles.portal_card}>
+        <Link to="/portal/1">
+          <PortalCard />
+        </Link>
+      </div>
+      <div className={styles.portal_details}>
+        <div className={styles.portal_title}>
+          <h1>Little Nightmares</h1>
+        </div>
+        <div className={styles.portal_stats}>
+          <div className={styles.stat}>
+            <div className={styles.data}>
+              <p>
+                100<span> xTGR</span>
+              </p>
+            </div>
+            <div className={styles.title}>
+              <p>Total NFTS</p>
+            </div>
+          </div>
+          <div className={styles.sub_stats}>
+            <div className={styles.stat}>
+              <div className={styles.data}>
+                <p>
+                  100<span> xTGR</span>
+                </p>
+              </div>
+              <div className={styles.title}>
+                <p>Total NFTS</p>
+              </div>
+            </div>
+            <div className={styles.seperator}></div>
+
+            <div className={styles.stat}>
+              <div className={styles.data}>
+                <p>
+                  100<span> xTGR</span>
+                </p>
+              </div>
+              <div className={styles.title}>
+                <p>Total NFTS</p>
+              </div>
+            </div>
+            <div className={styles.seperator}></div>
+            <div className={styles.stat}>
+              <div className={styles.data}>
+                <p>
+                  100<span> xTGR</span>
+                </p>
+              </div>
+              <div className={styles.title}>
+                <p>Total NFTS</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button className="btn-lg">Join Portal</button>
+      </div>
+    </div>
+  );
+};
+
+const PortalsList = () => {
+  return (
+    <div className={styles.portals_list_container}>
+      {[1, 2, 3, 4, 5].map((index) => (
+        <Link to="/portal/1">
+          <PortalCard />
+        </Link>
+      ))}
+    </div>
+  );
+};
