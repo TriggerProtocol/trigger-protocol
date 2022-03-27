e358958fe9b19db35845d4bda0d9a62bb67437d7
c0deb1ind31 <rhutikparab1@gmail.com>
2022-03-27 12:47:19 +0530

    Added Wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,27 @@ import { ExplorePortalsPage } from "pages/ExplorePortalsPage";
 import { Navbar } from "./Components/Navbar";
 import { Footer } from "./Components/Footer";
 import { PortalCard } from "./Components/PortalCard";
+import { providers } from "ethers";
+import { WagmiProvider } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+const connector = new InjectedConnector();
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="dev" element={<PortalCard />} />
-          <Route path="/portal/:portalId" element={<PortalPage />} />
-          <Route path="/explore-portals/" element={<ExplorePortalsPage />} />
-        </Routes>
-      </main>
-      {/* <Footer /> */}
-    </Router>
+    <WagmiProvider autoConnect={true} connectors={[connector]}>
+      <Router>
+        <Navbar />
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="dev" element={<PortalCard />} />
+            <Route path="/portal/:portalId" element={<PortalPage />} />
+            <Route path="/explore-portals/" element={<ExplorePortalsPage />} />
+          </Routes>
+        </main>
+        {/* <Footer /> */}
+      </Router>
+    </WagmiProvider>
   );
 }
 
