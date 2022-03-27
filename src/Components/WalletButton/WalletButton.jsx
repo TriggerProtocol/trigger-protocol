fd0deb3dceb3c473e69f9acced3001960bf4d0da
c0deb1ind31 <rhutikparab1@gmail.com>
2022-03-27 12:47:53 +0530

    Added Wallet Connection

--- src/Components/WalletButton/WalletButton.jsx
+++ src/Components/WalletButton/WalletButton.jsx
@@ -1,17 +1,28 @@
 import { Icon } from "@iconify/react";
 import React, { useEffect, useRef, useState } from "react";
+import { useConnect, useAccount } from "wagmi";
+import { PopupModal } from "Components/PopupModal";
 
 // styles
 import styles from "./WalletButton.module.scss";
 
 // assets
 import avatar from "../../assets/images/avatar.png";
-
+import metaMask from "assets/images/metamask.svg";
 export const WalletButton = () => {
   const wallet_ref = useRef(null);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [wallConnectToggle, setWalletConnectToggle] = useState(false);
+  const [{ data, error }, connect] = useConnect();
+  const [{ data: accountData }, disconnect] = useAccount({
+    fetchEns: true,
+  });
 
   useEffect(() => {
+    data.connected && setWalletConnectToggle(false);
+  }, [data.connected]);
+  useEffect(() => {
+    console.log(data.connectors);
     const handleClickOutisde = (event) => {
       if (wallet_ref.current && !wallet_ref.current.contains(event.target)) {
         setShowDropdown(false);
@@ -22,41 +33,75 @@ export const WalletButton = () => {
   }, []);
 
   return (
-    <div
-      className={styles.wallet_button}
-      ref={wallet_ref}
-      onClick={() => setShowDropdown(!showDropdown)}
-    >
+    <>
       <div
-        className={styles.wallet_state}
-        style={showDropdown ? { paddingTop: "100px" } : { paddingTop: "0px" }}
+        className={styles.wallet_button}
+        ref={wallet_ref}
+        onClick={() =>
+          data.connected
+            ? setShowDropdown(!showDropdown)
+            : setWalletConnectToggle(true)
+        }
       >
-        <div className={styles.avatar}>
-          <img src={avatar} alt="avatar" />
-        </div>{" "}
-        <div className={styles.wallet_details_wrapper}>
-          <div className={styles.wallet_details}>
-            <h4>0xEhs67........Tk2zw</h4>
-            <div className={styles.wallet_name}>
-              <p>Metamask Wallet</p>
-              <div className={styles.connected_or_not}></div>
-            </div>
+        <div
+          className={styles.wallet_state}
+          // style={showDropdown ? { paddingTop: "100px" } : { paddingTop: "0px" }}
+        >
+          {data.connected ? (
+            <>
+              <div className={styles.avatar}>
+                <img src={avatar} alt="avatar" />
+              </div>
+              <div className={styles.wallet_details_wrapper}>
+                <div className={styles.wallet_details}>
+                  <h4>{accountData.address}</h4>
+                  <div className={styles.wallet_name}>
+                    <p>{accountData.connector.name} Wallet</p>
+                    <div className={styles.connected_or_not}></div>
+                  </div>
+                </div>
+                <Icon
+                  icon="gridicons:dropdown"
+                  fontSize={"23px"}
+                  id={styles.dropdown_icon}
+                />
+              </div>{" "}
+            </>
+          ) : (
+            <h2>Connect Wallet</h2>
+          )}
+        </div>
+
+        {data.connected && showDropdown ? (
+          <div className={styles.dropdown}>
+            <p>Copy Address</p>
+            <p onClick={() => disconnect()}>Disconnect</p>
           </div>
-          <Icon
-            icon="gridicons:dropdown"
-            fontSize={"23px"}
-            id={styles.dropdown_icon}
-          />
-        </div>
+        ) : null}
       </div>
-      {showDropdown ? (
-        <div className={styles.dropdown}>
-          <p>Copy Address</p>
-          <p>Disconnect</p>
+      <PopupModal
+        modalTitle="Connect Wallet"
+        toggleModal={wallConnectToggle}
+        setToggleModal={(state) => setWalletConnectToggle(state)}
+      >
+        <div>
+          {data.connectors.map((connector) => (
+            <div
+              disabled={connector.ready === false || data.connected}
+              key={connector.id}
+              onClick={() => connect(connector)}
+              className={styles.connector_button}
+            >
+              <img src={metaMask} alt="" />
+              <h3> {connector.name}</h3>
+              {/* <p> {data.connected ? "Connected" : "connect"}</p> */}
+              {/* {!connector.ready && " (unsupported)"} */}
+            </div>
+          ))}
+
+          {/* {error && <div>{error?.message ?? "Failed to connect"}</div>} */}
         </div>
-      ) : (
-        ""
-      )}
-    </div>
+      </PopupModal>
+    </>
   );
 };
