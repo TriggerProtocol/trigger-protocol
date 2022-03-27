3ba50d2dea302b3467039a038dfd9264af605cde
c0deb1ind31 <rhutikparab1@gmail.com>
2022-03-27 17:17:07 +0530

    artifacts path fixed

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,10 +21,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    hardhat: {},
-    matic: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [privateKey],
-    },
+    // matic: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+  },
+  paths: {
+    // sources: "./contracts",
+    // tests: "./test",
+    // cache: "./cache",
+    artifacts: "./src/contracts/artifacts",
   },
 };
