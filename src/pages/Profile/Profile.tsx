a9c56ed69b0b713471dffbefae29fb74e488b234
c0deb1ind31 <rhutikparab1@gmail.com>
2022-04-03 16:53:02 +0530

    Added all games api

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAccount, useConnect } from "wagmi";
 // styles
 import styles from "./Profile.module.scss";
 import "styles/globals.css";
 import { PortalCard } from "Components/PortalCard";
 import { NFTcard } from "Components/NFTcard";
+import { createPortalInstance } from "configs/textile.io.configs";
+import { useTriggerProtocolContract } from "hooks";
 
+const axiosApiInstance = axios.create({
+  baseURL: "https://trigger-protocol-api.herokuapp.com/",
+});
 export const Profile = () => {
   const [currentNavState, setCurrentNavState] = useState(0);
 
@@ -77,9 +83,9 @@ const ProfileNavigator = ({
 const PortalsJoined = () => {
   return (
     <>
-      {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(() => (
+      {/* {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(() => (
         <PortalCard />
-      ))}
+      ))} */}
     </>
   );
 };
@@ -93,13 +99,67 @@ const MyNfts = () => {
     </>
   );
 };
-
+type gameData = {
+  name: string;
+  shortDescription: string;
+  gameThumbnail: string;
+};
 const MyGames = () => {
+  const [ownedGames, setOwnedGames] = useState<Array<gameData>>([]);
+  const [loading, setLoading] = useState(true);
+  const [{ data: accountData }, disconnect] = useAccount({
+    fetchEns: true,
+  });
+  const [{ data: walletConnection, error }] = useConnect();
+
+  const { createPortal } = useTriggerProtocolContract();
+  useEffect(() => {
+    const body = { userId: "76561199061887574" };
+    axios
+      .post(
+        "https://trigger-protocol-api.herokuapp.com/tgr-api/owned-games",
+        body
+      )
+      .then((data) => {
+        if (data.status === 200) {
+          setOwnedGames(data.data);
+          setLoading(false);
+        }
+      });
+  }, []);
+
+  //create new portal if portal not crreated
+  function handleCreatePortal(gameData: any) {
+    if (walletConnection.connected && accountData?.address) {
+      createPortalInstance(
+        gameData.name,
+        gameData.appId,
+        gameData.shortDescription,
+        gameData.gameThumbnail,
+        accountData?.address
+      ).then((data) => {
+        console.log(data);
+        // createPortal({ dbThreadID: data._id, appID: gameData.appId }).then(
+        //   () => {}
+        // );
+      });
+    }
+  }
   return (
     <>
-      {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(() => (
-        <PortalCard createPortal={true} />
-      ))}
+      {
+        loading ? "Loading" : console.log(ownedGames)
+        // ? ownedGames.map((data) => (
+        //     <PortalCard
+        //       gameTitle={data.name}
+        //       gameDescription={data.shortDescription}
+        //       gameThumbnail={data.gameThumbnail}
+        //       createPortal={true}
+        //       handleClick={() => handleCreatePortal(data)}
+        //     />
+        //   ))
+        // : "no games"
+      }
     </>
   );
 };
