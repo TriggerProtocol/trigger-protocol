da09bb58b536d31b652fb215d1834b181c286eb6
c0deb1ind31 <rhutikparab1@gmail.com>
2022-04-03 16:57:13 +0530

    minor change

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,10 +21,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    // matic: {
-    //   url: "https://rpc-mumbai.maticvigil.com",
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   paths: {
     // sources: "./contracts",
